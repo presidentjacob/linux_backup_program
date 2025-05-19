7950f53876ab01d5092f35926949b229d009cfc3
presidentjacob <jacobyepez04@gmail.com>
2025-05-18 23:23:46 -0600

    Fixed while loop in backup_library

--- backup_library.sh
+++ backup_library.sh
@@ -13,8 +13,8 @@ function display_directories {
 
 function read_input {
 	input=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-	while [[ $input != 'y' ]] && [[ $input != 'n' ]]; do
-		read "That is not a valid input, please enter a valid input [y/n]:" input
-		input=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+	while [[ $input != 'y' && $input != 'n' ]]; do
+		read -p "That is not a valid input, please enter a valid input [y/n]:" input
+		input=$(echo "$input" | tr '[:upper:]' '[:lower:]')
 	done
 }
